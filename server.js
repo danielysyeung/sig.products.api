ecc969d3bf438ccf10975919124f1878fb7ae8c1
danielysyeung <danielysyeung@gmail.com>
2016-10-03 18:25:25 -0700

    return correct response codes

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var mongodb = require("mongodb");
 var mongoClient = mongodb.MongoClient;
 var mongodbUrl = "mongodb://localhost:27017/sigdb";
 var db;
 
+app.use(bodyParser.json());
+
 mongoClient.connect(mongodbUrl, function(err, database) {  
   if (err) { 
     return console.log("Error connecting to mongodb: ", err); 
-  } else {
-    db = database;  
   }
+  db = database;  
 });
 
 app.get("/", function(req, res) {
   console.log("Got a GET request for the Products API");
   res.status(200);
-  res.send("Welcome to SIG Products API!");
+  return res.send("Welcome to SIG Products API!");
 });
 
 app.get("/products", function(req, res) {
@@ -25,6 +27,7 @@ app.get("/products", function(req, res) {
   var queryBySku;  
   var queryResult;
   
+  // TODO filtering, sorting, pagination
   if (req.query.sku && req.query.sku.trim()) {
     queryBySku = { "sku":req.query.sku };  
   } 
@@ -32,18 +35,20 @@ app.get("/products", function(req, res) {
   var collection = db.collection("product", function(err, collection) {
     if (err) {
 	  console.log("Error accessing collection: ", err);
-	} else {
-	  collection.find(queryBySku, fieldProjection).toArray(function(err, docs) {
-        if (err) {
-	      console.log("Error querying document: ", err);
-	    } else {
-		  console.log("Found " + docs.length + " documents.");
-		  res.status(200);
-		  res.send(docs);
-        } 
-	  })
-	}
-  });      
+      res.status(500);
+      return res.send("");
+	} 
+	collection.find(queryBySku, fieldProjection).toArray(function(err, docs) {
+      if (err) {
+        console.log("Error querying document: ", err);
+        res.status(500);
+        return res.send("");
+	  } 
+	  console.log("Found " + docs.length + " documents.");
+	  res.status(200);
+	  return res.send(docs);
+    });
+  });
 });
 
 app.get("/products/:sku", function(req, res) {   
@@ -54,51 +59,127 @@ app.get("/products/:sku", function(req, res) {
   var collection = db.collection("product", function(err, collection) {
     if (err) {
 	  console.log("Error accessing collection: ", err);
-	} else {
-	  collection.findOne(queryBySku, fieldProjection, function(err, doc) {
-        if (err) {
-	      console.log("Error querying document: ", err);
-	    } else {
-		  res.status(200);
-		  res.send(doc);
-        } 
-	  })
-	}
+      res.status(500);
+      return res.send("");
+	} 
+	collection.findOne(queryBySku, fieldProjection, function(err, doc) {
+      if (err) {
+        console.log("Error querying document: ", err);
+        res.status(500);
+        return res.send("");
+	  } 
+	  if (!doc) {
+		res.status(404);
+		return res.send("");
+      } 
+	  res.status(200);
+	  return res.send(doc);	
+	});
   });      
 });
 
 app.post("/products", function(req, res) {
-  console.log("Got a POST request for /products");
+  var body = req.body;
+  console.log("Got a POST request for /products: %j", req.body);   
+
+  if (!req.body.sku || !req.body.sku.trim() || !req.body.name || !req.body.name.trim()) {
+    res.status(400);
+	return res.send("");
+  }  
+  
+  var keyDoc = { "sku":req.body.sku };
+  var valueDoc = { "sku":req.body.sku, "name":req.body.name, "description":req.body.description } 
   var collection = db.collection("product", function(err, collection) {
     if (err) {
 	  console.log("Error accessing collection: ", err);
-	}
-  });  
-  collection.updateOne(
-    { "sku":"10006" },
-	{ $set:{ "sku":"10006", "name":"product 10006", "description":"product description 10006yyy" }, "$currentDate":{ "lastUpdatedTimestamp":true } },
-	{ "upsert":true },
-	function(err, result) {
+      res.status(500);
+      return res.send("");
+    } 
+	collection.updateOne(keyDoc, { $set:valueDoc, "$currentDate":{ "lastUpdatedTimestamp":true } }, { "upsert":true }, function(err, result) {
 	  if (err) {
 	    console.log("Error upserting document: ", err);
-	  } else {
-	    res.status(201);
-	    res.send("");
-      }
-	}
-  );  
+        res.status(500);
+        return res.send("");
+	  }
+      res.location("/products/" + req.body.sku);
+	  res.status(201);
+	  return res.send("");       
+	});  
+  });  
 });
 
 app.put("/products/:sku", function(req, res) {
-  console.log("Got a PUT request for /products/");
-  var sku = req.params.sku;
-  res.send("Hello PUT /products/ for sku=" + sku);
+  var body = req.body;
+  console.log("Got a PUT request for /products/: %j", req.body);
+
+  if (!req.body.sku || !req.body.sku.trim() || !req.body.name || !req.body.name.trim()) {
+    res.status(400);
+	return res.send("");
+  }  
+
+  var queryBySku = { "sku":req.params.sku };  
+  var valueDoc = { "sku":req.body.sku, "name":req.body.name, "description":req.body.description } 
+  var queryResult;
+  var collection = db.collection("product", function(err, collection) {
+    if (err) {
+	  console.log("Error accessing collection: ", err);
+	  res.status(500);
+	  return res.send("");
+	} 
+	collection.findOne(queryBySku, function(err, doc) {
+      if (err) {
+	    console.log("Error querying document: ", err);
+        res.status(500);
+        return res.send("");
+	  } 
+	  if (!doc) {
+		res.status(404);
+		return res.send("");
+      } 
+	  collection.updateOne(queryBySku, { $set:valueDoc, "$currentDate":{ "lastUpdatedTimestamp":true } }, { "upsert":true }, function(err, result) {
+	    if (err) {
+	      console.log("Error upserting document: ", err);
+		  res.status(500);
+		  return res.send("");
+	    }
+		res.status(204);
+        return res.send("");
+      });  
+    });
+  });      
 });
 
 app.delete("/products/:sku", function (req, res) {
   console.log("Got a DELETE request for /products/");
-  var sku = req.params.sku;
-  res.send("Hello DELETE /products/ for sku=" + sku);
+  var queryBySku = { "sku":req.params.sku };  
+  var queryResult;
+  var collection = db.collection("product", function(err, collection) {
+    if (err) {
+	  console.log("Error accessing collection: ", err);
+	  res.status(500);
+	  return res.send("");
+	} 
+	collection.findOne(queryBySku, function(err, doc) {
+      if (err) {
+	    console.log("Error querying document: ", err);
+        res.status(500);
+        return res.send("");
+	  }
+	  if (!doc) {
+	    res.status(404);
+	    return res.send("");
+      }
+      collection.remove(queryBySku, { "justOne":true }, function(err, result) {
+	    if (err) {
+	      console.log("Error removing document: ", err);
+		  res.status(500);
+		  return res.send("");
+	    }
+	    res.status(200);
+	    return res.send("");
+      });
+    });	
+  });  
 });
 
 var port = process.env.PORT || 8080;
